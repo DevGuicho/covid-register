78fbb441891ebcb552a3286f9e447f0353d5d9b3
Luis Vazquez <luvazpa@gmail.com>
2021-06-07 17:23:52 -0500

    crud added

--- src/controllers/patients.js
+++ src/controllers/patients.js
@@ -7,7 +7,7 @@ exports.addPatient = (req, res) => {
   res.render('pages/addPatient')
 }
 
-exports.listPatients = async (req, res) => {
+exports.find = async (req, res) => {
   res.json({
     data: await Patient.find({})
   })
@@ -17,4 +17,36 @@ exports.findOneById = async (req, res) => {
   res.json({
     data: await Patient.findById(id)
   })
+}
+exports.create = async (req, res) => {
+  const patient = req.query
+  const parsedPatient = parsePatient(patient)
+  const newPatient = new Patient(parsedPatient)
+
+  res.json({
+    data: await newPatient.save()
+  })
+}
+exports.update = async (req, res) => {
+  const patient = req.query
+  const { id } = req.params
+  const parsedPatient = parsePatient(patient)
+
+  res.json({
+    data: await Patient.findByIdAndUpdate(id, parsedPatient, { new: true })
+  })
+}
+
+exports.delete = async (req, res) => {
+  const { id } = req.params
+  res.json({
+    data: await Patient.findByIdAndDelete(id)
+  })
+}
+
+const parsePatient = (patient) => {
+  const age = parseInt(patient.age)
+  const isIntubated = patient.isIntubated === 'true'
+  const gurney = parseInt(patient.gurney)
+  return { ...patient, age, isIntubated, gurney }
 }
--- src/routes/patients.js
+++ src/routes/patients.js
@@ -10,12 +10,18 @@ router.get('/', patientsController.patients)
 router.get('/add-patient', patientsController.addPatient)
 
 // Esta ruta muestra todos los registros en formato JSON
-router.get('/api/patients', patientsController.listPatients)
+router.get('/api/patients', patientsController.find)
 
 // Esta ruta muestra un registro en formato JSON
 router.get('/api/patients/:id', patientsController.findOneById)
 
-// Esta ruta muestra un registro en formato JSON
-router.post('/api/patients/', patientsController.findOneById)
+// Esta ruta crea un nuevo registro de paciente
+router.post('/api/patients', patientsController.create)
+
+// Esta ruta actualiza un nuevo registro de paciente
+router.put('/api/patients/:id', patientsController.update)
+
+// Esta ruta actualiza un nuevo registro de paciente
+router.delete('/api/patients/:id', patientsController.delete)
 
 module.exports = router
